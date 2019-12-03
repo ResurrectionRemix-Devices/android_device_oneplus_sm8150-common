aa69f347f7f9ec8a0ebf684790bb11884aaf74f6
maxwen <max.weninger@gmail.com>
2019-12-03 22:59:42 +0100

    sm8150-common: add mock implemention of OpDeviceManagerInjector
    prevents some oneplus prop apps to crash trying to access their 'spyware'
    e.g. the dolby atmos app
    
    Change-Id: Iecb52805d322890ffee977c9419dc44b1430f600
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- common.mk
+++ common.mk
@@ -171,6 +171,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+PRODUCT_PACKAGES += oneplus-mock
+PRODUCT_BOOT_JARS += oneplus-mock
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
