20129df088ff0fef7edc2cf911ea2328b715926e
Marko Man <darkobas@gmail.com>
2020-04-05 00:17:44 +0200

    hotdog: fod, fix dimAmount
    Change-Id: I180e63b76dc28baba1f476cfab4da7990481764a
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -45,6 +45,8 @@ namespace inscreen {
 namespace V1_0 {
 namespace implementation {
 
+int dimAmount;
+
 /*
  * Write value to path and close file.
  */
@@ -84,7 +86,7 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 
 Return<void> FingerprintInscreen::onPress() {
     this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 2);
     set(HBM_ENABLE_PATH, 1);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
 
@@ -93,7 +95,7 @@ Return<void> FingerprintInscreen::onPress() {
 
 Return<void> FingerprintInscreen::onRelease() {
     this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 5);
     set(HBM_ENABLE_PATH, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
@@ -103,6 +105,8 @@ Return<void> FingerprintInscreen::onRelease() {
 Return<void> FingerprintInscreen::onShowFODView() {
     this->mFodCircleVisible = true;
 
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 2);
+    //set(HBM_DIM_PATH, getDimAmount(255));
     return Void();
 }
 
@@ -154,10 +158,21 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
-    int dimAmount = get(DIM_AMOUNT_PATH, 0);
-    LOG(INFO) << "dimAmount = " << dimAmount;
-
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t suggest) {
+    LOG(INFO) << "dimAmount1 = " << suggest;
+    if (suggest > 0) {
+        if (suggest < 127) {
+            dimAmount = 255 - suggest - 16;
+        } else {
+            dimAmount = 92 + get(DIM_AMOUNT_PATH, suggest);
+            if (dimAmount > 237) {
+                dimAmount = 255;
+            }
+        }
+    } else {
+        dimAmount = 180;
+    }
+    LOG(INFO) << "dimAmount2 = " << dimAmount;
     return dimAmount;
 }
 
