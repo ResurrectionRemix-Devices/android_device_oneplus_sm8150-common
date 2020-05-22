9c1fe8cedca74fb13a6306a053b0d2c8405e048d
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-05-22 11:40:33 +0300

    sm8150-common: fod: Set dim mode on show of fod view
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -100,12 +100,7 @@ Return<bool> FingerprintInscreen::noDim() {
     return true;
 }
 
-Return<void> FingerprintInscreen::switchHbm(bool enabled) {
-    if (enabled) {
-        this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
-    } else {
-        this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
-    }
+Return<void> FingerprintInscreen::switchHbm(bool) {
     return Void();
 }
 
@@ -123,6 +118,7 @@ Return<void> FingerprintInscreen::onRelease() {
 
 Return<void> FingerprintInscreen::onShowFODView() {
     this->mFodCircleVisible = true;
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
 
     return Void();
 }
