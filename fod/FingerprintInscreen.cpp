dc02a391f6e536ede178c592418e19d7fcc94466
Lup Gabriel <gwolf2u.website@gmail.com>
2020-05-31 17:40:18 +0300

    sm8150-common: revert FOD HAL 1.1 modifications
    Revert "sm8150-common: remove obsolete FOD HAL"
    This reverts commit a533467976df10bd10eecc0699a0338225c3fb54.
    
    Revert "sm8150-common: fod: Clean and update"
    This reverts commit 73c87e4287dd6a4ec0d60a4edab931db69936617.
    
    Revert "sm8150-common: fod: bump hal to version 1.1"
    This reverts commit 43657cc7ee19a0ffdddafd299500f62fb7e3d60a.
    
    Revert "guacamoleb: fod: Calculate dim alpha based on real brightness"
    This reverts commit 1b15a52bb8bb3217140c4dd8d23096a77867ef81.
    
    Revert "sm8150-common: fod: Hardcode dim amount for hbm mode(670)"
    This reverts commit a684964d8ca922773148b87fd737f990768d8303.
    
    Revert "Revert "sm8150-common: fod: Disable DC-Dimming when fod circle is visible""
    This reverts commit e5efe024f41b0ded051b89abba75a6f78da72429.
    
    Revert "sm8150-common: fod: Disable DC-Dimming when fod circle is visible"
    This reverts commit df3b821efcce81db857bcb369d3e3cf29a51c8fc.
    
    Revert "sm8150-common: squash revert FOD commits"
    This reverts commit e6eb8d74d2fada278ee91cee1c0043973bfae74b.
    
    Revert "sm8150-common: fod: Correct dim amount for HBM mode"
    This reverts commit fc90691c857ffa79a32a42e3806f024972d62c88.
    
    Revert "sm8150-common: fod: Use new FOD dimming alpha node"
    This reverts commit bf17b95c8770603bca3125f3b4000faca4381aca.
    
    Revert "sm8150-common: fod: Disable DC-Dimming when fod circle is visible"
    This reverts commit 551d895c23dedb7cfab78d49b31077e398b22fdc.
    
    Revert "sm8150-common: set TARGET_USES_FOD_HACK"
    This reverts commit e2c84c1c981d63fcaeee020eff082da807d248a0.
    
    sm8150-common: fod: Hardcode dim amount for hbm mode(670)
    Change-Id: Ic403cbd69a57d2da78e388cfc31d0aa8dbb0d8e1
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -19,9 +19,9 @@
 #include "FingerprintInscreen.h"
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
-#include <cmath>
 #include <fstream>
 
+#define FINGERPRINT_ERROR_CANCELED 5
 #define FINGERPRINT_ACQUIRED_VENDOR 6
 #define FINGERPRINT_ERROR_VENDOR 8
 
@@ -34,17 +34,12 @@
 #define OP_DISPLAY_NOTIFY_PRESS 9
 #define OP_DISPLAY_SET_DIM 10
 
-#define HBM_OFF_DELAY 0
-#define HBM_ON_DELAY 0
-
-#define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/dim_alpha"
-
 namespace vendor {
 namespace lineage {
 namespace biometrics {
 namespace fingerprint {
 namespace inscreen {
-namespace V1_1 {
+namespace V1_0 {
 namespace implementation {
 
 /*
@@ -67,11 +62,13 @@ static T get(const std::string& path, const T& def) {
 
 FingerprintInscreen::FingerprintInscreen() {
     this->mFodCircleVisible = false;
+    this->mIsEnrolling = false;
     this->mVendorFpService = IVendorFingerprintExtensions::getService();
     this->mVendorDisplayService = IOneplusDisplay::getService();
 }
 
 Return<void> FingerprintInscreen::onStartEnroll() {
+    this->mIsEnrolling = true;
     this->mVendorFpService->updateStatus(OP_DISABLE_FP_LONGPRESS);
     this->mVendorFpService->updateStatus(OP_RESUME_FP_ENROLL);
 
@@ -79,43 +76,23 @@ Return<void> FingerprintInscreen::onStartEnroll() {
 }
 
 Return<void> FingerprintInscreen::onFinishEnroll() {
+    this->mIsEnrolling = false;
     this->mVendorFpService->updateStatus(OP_FINISH_FP_ENROLL);
 
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getHbmOffDelay() {
-    return HBM_OFF_DELAY;
-}
-
-Return<int32_t> FingerprintInscreen::getHbmOnDelay() {
-    return HBM_ON_DELAY;
-}
-
-Return<bool> FingerprintInscreen::supportsAlwaysOnHBM() {
-    return true;
-}
-
-Return<bool> FingerprintInscreen::noDim() {
-    return true;
-}
-
-Return<void> FingerprintInscreen::switchHbm(bool enabled) {
-    if (enabled) {
-        this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
-    } else {
-        this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
-    }
-    return Void();
-}
-
 Return<void> FingerprintInscreen::onPress() {
+    if (mIsEnrolling) {
+        this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    }
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
 
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
@@ -132,6 +109,7 @@ Return<void> FingerprintInscreen::onShowFODView() {
 
 Return<void> FingerprintInscreen::onHideFODView() {
     this->mFodCircleVisible = false;
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
@@ -166,7 +144,18 @@ Return<bool> FingerprintInscreen::handleAcquired(int32_t acquiredInfo, int32_t v
 }
 
 Return<bool> FingerprintInscreen::handleError(int32_t error, int32_t vendorCode) {
-    return error == FINGERPRINT_ERROR_VENDOR && vendorCode == 6;
+    switch (error) {
+        case FINGERPRINT_ERROR_CANCELED:
+            if (vendorCode == 0) {
+                this->mIsEnrolling = false;
+            }
+            return false;
+        case FINGERPRINT_ERROR_VENDOR:
+            // Ignore vendorCode 6
+            return vendorCode == 6;
+        default:
+            return false;
+    }
 }
 
 Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
@@ -176,21 +165,8 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
-    int dimAmount;
-    float alpha;
-    int realBrightness = brightness * 2047 / 255;
-
-    if (realBrightness > 500) {
-        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.455);
-    } else {
-        alpha = 1.0 - pow(realBrightness / 1680.0, 0.455);
-    }
-    dimAmount = 255 * alpha;
-
-    LOG(INFO) << "dimAmount = " << dimAmount;
-
-    return dimAmount;
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
+    return 0;
 }
 
 Return<bool> FingerprintInscreen::shouldBoostBrightness() {
@@ -219,9 +195,10 @@ Return<int32_t> FingerprintInscreen::getSize() {
 }
 
 }  // namespace implementation
-}  // namespace V1_1
+}  // namespace V1_0
 }  // namespace inscreen
 }  // namespace fingerprint
 }  // namespace biometrics
 }  // namespace lineage
 }  // namespace vendor
+
